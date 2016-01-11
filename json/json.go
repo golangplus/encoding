3a891ad98b25d5ad51cd380b451870791a8e0435
David Deng <daviddengcn@gmail.com>
2016-01-10 23:25:21 -0800

    ADD read from file and write to file funcs for json codec

---
+++ json/json.go
@@ -0,0 +1,26 @@
+package jsonp
+
+import (
+	"encoding/json"
+	"os"
+)
+
+// MarshalToFile encodes a data into a JSON file.
+func MarshalToFile(v interface{}, filename string) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return json.NewEncoder(f).Encode(v)
+}
+
+// UnmarshalFile decodes a JSON from a file.
+func UnmarshalFile(filename string, v interface{}) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return json.NewDecoder(f).Decode(v)
+}
---
+++ json/json_test.go
@@ -0,0 +1,50 @@
+package jsonp
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/golangplus/testing/assert"
+)
+
+type T struct {
+	S string
+}
+
+func TestMarshalToFile(t *testing.T) {
+	fn := path.Join(os.TempDir(), "TestMarshalToFile.json")
+	s := T{
+		S: "abc",
+	}
+	assert.NoError(t, MarshalToFile(s, fn))
+	bs, err := ioutil.ReadFile(fn)
+	assert.NoError(t, err)
+	assert.Equal(t, "bs", strings.TrimSpace(string(bs)), `{"S":"abc"}`)
+}
+
+func TestMarshalToFile_Failed(t *testing.T) {
+	fn := path.Join(os.TempDir(), strconv.FormatInt(time.Now().UnixNano(), 16), "TestMarshalToFile.json")
+	s := T{
+		S: "abc",
+	}
+	assert.Error(t, MarshalToFile(s, fn))
+}
+
+func TestUnmarshalFile(t *testing.T) {
+	fn := path.Join(os.TempDir(), "TestMarshalToFile.json")
+	assert.NoError(t, ioutil.WriteFile(fn, []byte(`{"S":"abc"}`), 0644))
+	var s T
+	assert.NoError(t, UnmarshalFile(fn, &s))
+	assert.Equal(t, "s.S", s.S, "abc")
+}
+
+func TestUnmarshalFile_Failed(t *testing.T) {
+	fn := path.Join(os.TempDir(), strconv.FormatInt(time.Now().UnixNano(), 16), "TestMarshalToFile.json")
+	var s T
+	assert.Error(t, UnmarshalFile(fn, &s))
+}
